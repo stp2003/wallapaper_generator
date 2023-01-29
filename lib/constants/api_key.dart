c0f53613b5b49b9b5eb6a6dbd6960c1c9bc6c563
stp2003 <100586951+stp2003@users.noreply.github.com>
2023-01-29 17:21:28 +0530

    Initial commit

---
+++ lib/constants/api_key.dart
@@ -0,0 +1,1 @@
+const apiKey = 'D3m8ZZPF9pcLWIlJo9dCojQZp4nHNWhpImrIRS1KtFLyG1BvGnUTDHWI';
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:wallapaper_generator/screens/home_screen.dart';
+
+void main() {
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'WallVortex Hub',
+      theme: ThemeData.dark().copyWith(),
+      home: const HomePage(),
+    );
+  }
+}
